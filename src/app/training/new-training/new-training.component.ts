9429fee49d887de6ec562d63bf52d1deeeb62f20
Anthony Santiago <asantiago323@gmail.com>
2018-12-19 08:38:31 -0500

    started using firebase to read and write data

--- src/app/training/new-training/new-training.component.ts
+++ src/app/training/new-training/new-training.component.ts
@@ -1,23 +1,35 @@
-import { Component, OnInit } from "@angular/core";
-import { Exercise } from "../exercise.model";
-import { ExerciseService } from "../exercise.service";
-import { NgForm } from "@angular/forms";
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Exercise } from '../exercise.model';
+import { ExerciseService } from '../exercise.service';
+import { NgForm } from '@angular/forms';
+import { Subscription } from 'rxjs';
 
 @Component({
-  selector: "app-new-training",
-  templateUrl: "./new-training.component.html",
-  styleUrls: ["./new-training.component.scss"]
+  selector: 'app-new-training',
+  templateUrl: './new-training.component.html',
+  styleUrls: ['./new-training.component.scss']
 })
-export class NewTrainingComponent implements OnInit {
+export class NewTrainingComponent implements OnInit, OnDestroy {
   exercises: Exercise[];
+  exerciseSubscription: Subscription;
 
-  constructor(private exerciseService: ExerciseService) {
-    this.exercises = this.exerciseService.getExercises();
+  constructor(private exerciseService: ExerciseService) {}
+
+  ngOnInit() {
+    this.exerciseSubscription = this.exerciseService.exercisesChanged.subscribe(
+      exercises => {
+        this.exercises = exercises;
+        console.log('exercises: ', this.exercises);
+      }
+    );
+    this.exerciseService.fetchAvailableExercises();
   }
-
-  ngOnInit() {}
 
   onStartTraining(form: NgForm) {
     this.exerciseService.startExercise(form.value.exercise);
+  }
+
+  ngOnDestroy() {
+    this.exerciseSubscription.unsubscribe();
   }
 }
