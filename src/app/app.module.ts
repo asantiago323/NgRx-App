9478cbce2c0a5aeb8b7b0060dec10c59ba4f6bd2
Anthony Santiago <Anthony.Santiago@caci.com>
2018-12-19 09:39:33 -0500

    small patch to package.json

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,24 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { FlexLayoutModule } from "@angular/flex-layout";
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
-import { AppComponent } from "./app.component";
-import { MaterialModule } from "./material.module";
-import { LoginComponent } from "./auth/login/login.component";
-import { SignupComponent } from "./auth/signup/signup.component";
-import { TrainingComponent } from "./training/training.component";
-import { PastTrainingComponent } from "./training/past-training/past-training.component";
-import { CurrentTrainingComponent } from "./training/current-training/current-training.component";
-import { NewTrainingComponent } from "./training/new-training/new-training.component";
-import { WelcomeComponent } from "./welcome/welcome.component";
-import { AppRoutingModule } from "./app-routing.module";
-import { FormsModule, ReactiveFormsModule } from "@angular/forms";
-import { HeaderComponent } from "./navigation/header/header.component";
-import { SidenavListComponent } from "./navigation/sidenav-list/sidenav-list.component";
-import { StopTrainingComponent } from "./training/current-training/stop-training.component";
-import { AuthService } from "./auth/auth.service";
-import { ExerciseService } from "./training/exercise.service";
+import { AppComponent } from './app.component';
+import { MaterialModule } from './material.module';
+import { LoginComponent } from './auth/login/login.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { TrainingComponent } from './training/training.component';
+import { PastTrainingComponent } from './training/past-training/past-training.component';
+import { CurrentTrainingComponent } from './training/current-training/current-training.component';
+import { NewTrainingComponent } from './training/new-training/new-training.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { AppRoutingModule } from './app-routing.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HeaderComponent } from './navigation/header/header.component';
+import { SidenavListComponent } from './navigation/sidenav-list/sidenav-list.component';
+import { StopTrainingComponent } from './training/current-training/stop-training.component';
+import { AuthService } from './auth/auth.service';
+import { ExerciseService } from './training/exercise.service';
 
 @NgModule({
   declarations: [
